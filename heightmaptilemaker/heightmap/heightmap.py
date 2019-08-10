62266d5956c4896cf68fccf4854497d935b6032d
Ulrich Johan Isachsen <ulrich@isachsen.org>
2019-08-10 11:35:46 +0200

    Add heightmap and heightmap maker
    The heightmap class is able to create simple image-like heightmaps from
    raster files. This is done by iterating over all the geo-coordinates that
    is represented by each pixel. This is the reason for providing
    geo-transformation for the heightmap.
    
    Example code is provided through a runnable python file that can create a
    heightmap by specifying heightmap width, height and the geo-locations of
    the top-left and top right corner of the image together with the geotiff
    files.
    
    numpy dependency added
    
    matplotlib.pyplot dependencies are added, but is only required if the plot
    argument is provided to the make_heightmap.py program.

---
+++ heightmaptilemaker/heightmap/heightmap.py
@@ -0,0 +1,65 @@
+import geo.geo_utils
+import geo.raster_lookup
+
+import glob
+import numpy as np
+
+class Heightmap:
+    def __init__(self):
+        self.pixels = []
+        self.heightmap = None
+        self.nodata_fillin = 0
+
+        self.out_of_bounds_count = 0
+        self.nodata_count = 0
+
+    def createFromRaster(self, raster_lookup, geo_transform, heightmap_size):
+        self.pixels = [0 for i in range(heightmap_size[0] * heightmap_size[1])]
+
+        for y in range(heightmap_size[1]):
+            for x in range(heightmap_size[1]):
+                geo_pos = geo_transform.transformPixelLocationToGeoLocation(x, y)
+                pixel_index = x + y*heightmap_size[0]
+                if raster_lookup.locationInBounds(geo_pos[0], geo_pos[1]):
+                    elevation = raster_lookup.getElevationAtPosition(geo_pos[0], geo_pos[1])
+                    if elevation is not None:
+                        self.pixels[pixel_index] = elevation
+                    else:
+                        self.pixels[pixel_index] = self.nodata_fillin
+                        self.nodata_count += 1
+                else:
+                    self.out_of_bounds_count += 1
+
+        raster_matrix = np.array(self.pixels).reshape(heightmap_size)
+        self.heightmap = raster_matrix
+
+        return self
+
+    def pixelCount(self):
+        return self.heightmap.shape[0] * self.heightmap.shape[1]
+
+    def getStatistics(self):
+        return {
+            'out_of_bounds_percentage': 100.0 * float(self.out_of_bounds_count) / self.pixelCount(),
+            'nodata_percentage': 100.0 * float(self.nodata_count) / self.pixelCount()
+        }
+
+    def loadFromFile(self, file_name):
+        self.heightmap = np.load(file_name)
+        self.pixels = list(self.heightmap.reshape(self.heightmap.shape[0] * self.heightmap.shape[1]))
+
+        return self
+
+    def writeToFile(self, file_name):
+        if self.heightmap is None or len(self.pixels) <= 0:
+            raise Exception("Heigtmap is not loaded")
+        np.save(file_name, self.heightmap)
+
+    def getWidth(self):
+        return self.heightmap.shape[0]
+
+    def getHeight(self):
+        return self.heightmap.shape[1]
+
+    def getHeightmap(self):
+        return self.heightmap
---
+++ heightmaptilemaker/make_heightmap.py
@@ -0,0 +1,56 @@
+from geo import geo_utils
+from geo import raster_lookup
+from heightmap import heightmap
+
+import argparse
+
+if __name__ == '__main__':
+    argument_parser = argparse.ArgumentParser(description='Program will create a numpy array heightmap from the specified geotiff files and image top row in geo-coordinates')
+    argument_parser.add_argument('top_left_corner_geo_east', metavar='top-left-corner-geo-east', type=float, help='The east geo coordinate of the top left corner of the heightmap image')
+    argument_parser.add_argument('top_left_corner_geo_north', metavar='top-left-corner-geo-north', type=float, help='The north geo coordinate of the top left corner of the heightmap image')
+    argument_parser.add_argument('top_right_corner_geo_east', metavar='top-right-corner-geo-east', type=float, help='The east geo coordinate of the top right corner of the heightmap image')
+    argument_parser.add_argument('top_right_corner_geo_north', metavar='top-right-corner-geo-north', type=float, help='The north geo coordinate of the top right corner of the heightmap image')
+    argument_parser.add_argument('geo_tiff_files', metavar='geo-tiff-files', type=str, nargs='+', help='Specify a list of geotiff files to create heightmap from')
+    argument_parser.add_argument('--heightmap-width', type=int, default=1024, help='Specify the preferred width of the heightmap you want to create (default=1024)')
+    argument_parser.add_argument('--heightmap-height', type=int, default=1024, help='Specify the preferred height of the heightmap you want to create (default=1024)')
+    argument_parser.add_argument('--plot', action='store_true', help='Draw the resulting heightmap using pyplot')
+    argument_parser.add_argument('--output', type=str, help='Specify the file of the result (npy file)')
+    arguments = argument_parser.parse_args()
+
+    geo_top_left_x = arguments.top_left_corner_geo_east
+    geo_top_left_y = arguments.top_left_corner_geo_north
+    geo_top_right_x = arguments.top_right_corner_geo_east
+    geo_top_right_y = arguments.top_right_corner_geo_north
+
+    heightmap_width = arguments.heightmap_width
+    heightmap_height = arguments.heightmap_height
+
+    geo_top_left = (geo_top_left_x, geo_top_left_y)
+    geo_top_right = (geo_top_right_x, geo_top_right_y)
+    heightmap_size = (heightmap_width, heightmap_height)
+
+    heightmap_geo_transform = geo_utils.computeGdalGeoTransformFrom2Points(
+        geo_top_left, geo_top_right, heightmap_size)
+
+    print('Got the following GDAL geo transform parameters: ' + str(heightmap_geo_transform.transform_parameters))
+
+    print('Loading geo files')
+    geo_tiff_files = arguments.geo_tiff_files
+    raster_lookup = raster_lookup.RasterLookup(geo_tiff_files)
+
+    print('Creating heightmap')
+    heightmap = heightmap.Heightmap().createFromRaster(raster_lookup, heightmap_geo_transform, heightmap_size)
+
+    print('Stats: ' + str(heightmap.getStatistics()))
+
+    if arguments.output is not None:
+        print('Saving heightmap to ' + str(arguments.output))
+        output_file = arguments.output
+        heightmap.writeToFile(output_file)
+
+    if arguments.plot is not None:
+        import matplotlib.pyplot as plt
+
+        print('Plotting image')
+        plt.imshow(heightmap.getHeightmap())
+        plt.show()
